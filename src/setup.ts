fd13b3838adf56396087da1e35325b7108f16d10
Ashish Bhagat <ashishbhagat@Ashishs-MacBook-Air.local>
2025-06-04 13:12:07 +0530

    fixed light intensity on surface

--- src/setup.ts
+++ src/setup.ts
@@ -40,7 +40,7 @@ export function initCore(): Core {
         }
     );
     world.defaultContactMaterial = defaultContactMaterial;
-    world.addContactMaterial(defaultContactMaterial);
+    world.addContactMaterial(defaultContactMaterial);  // Make sure it's added
 
     const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
     camera.position.set(-25.5, 46.5, 49.5);
