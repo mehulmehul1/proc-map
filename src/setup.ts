8b533859d483cb19d11eccf1bd0849ea2c94cab5
Ashish Bhagat <ashishbhagat@Ashishs-MacBook-Air.local>
2025-06-04 13:10:44 +0530

    fixed light intensity on surface

--- src/setup.ts
+++ src/setup.ts
@@ -56,8 +56,8 @@ export function initCore(): Core {
     if (!appElement) throw new Error("Could not find #app element");
     appElement.appendChild(renderer.domElement);
 
-    const light = new THREE.PointLight(new THREE.Color("#FFCB8E").convertSRGBToLinear(), 1700, 8000);
-    light.position.set(10, 20, 10);
+    const light = new THREE.PointLight(new THREE.Color("#FFCB8E").convertSRGBToLinear(),5000, 5000);
+    light.position.set(10, 30, 10);
     light.castShadow = true;
     light.shadow.mapSize.width = 512;
     light.shadow.mapSize.height = 512;
